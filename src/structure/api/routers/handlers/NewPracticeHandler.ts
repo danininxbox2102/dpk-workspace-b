af5267765aeffa25b9af4d5dc8970e1a4ab2669e
Daninin <danininps@yandex.ru>
2025-10-28 01:51:48 +0300

    503 in addPractice.ts

---
+++ src/structure/api/routers/handlers/NewPracticeHandler.ts
@@ -0,0 +1,15 @@
+import IApiHandler from "@/interfaces/IApiHandler";
+import {Request, Response} from "express";
+import RestError from "@/config/entities/RestError";
+
+export default class NewPracticeHandler implements IApiHandler {
+
+    async handle(req: Request, res: Response) {
+        const jsonBody = req.body;
+        if (!jsonBody){
+            return res.status(400).json(new RestError("NO_DATA", "You provided empty body"))
+        }
+
+        return res.status(503).send("This function isnt working now. Its under maintance. Please wait. Coded with Love TerComPy (SIP-24-2)")
+    }
+}
--- src/structure/api/routers/mainRouter.ts
+++ src/structure/api/routers/mainRouter.ts
@@ -2,16 +2,21 @@ import express, {Router} from "express";
 import ExampleHandler from "@/structure/api/routers/handlers/ExampleHandler";
 import DocumentHandler from "@/structure/api/routers/handlers/DocumentHandler";
 import PracticeListHandler from "@/structure/api/routers/handlers/PracticeListHandler";
-import addPractice from "./handlers/addPractice";
+import addPractice from "./handlers/NewPracticeHandler";
 
 const router:Router = express.Router();
 export const mainRouter = router;
 
-// Example route with handler
+// Main router
+
+//
 router.get('/', new ExampleHandler().handle)
 
+// Get document by its name
 router.get('/document/:name', new DocumentHandler().handle)
 
+// Get practice list
 router.get('/practices', new PracticeListHandler().handle)
 
+// Post new practice
 router.post('/practice/new', new addPractice().handle)
